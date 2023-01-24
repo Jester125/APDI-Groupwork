cae4e177e47f0e3e27fdced2d53c390fe65acbfe
Dougflas <123095041+Dougflas@users.noreply.github.com>
2023-01-24 11:25:21 +0000

    .h v1.0

---
+++ EffectPlugin.h
@@ -0,0 +1,61 @@
+//
+//  EffectPlugin.h
+//  MyEffect Plugin Header File
+//
+//  Used to declare objects and data structures used by the plugin.
+//
+
+#pragma once
+
+#include "apdi/Plugin.h"
+#include "apdi/Helpers.h"
+using namespace APDI;
+
+#include "EffectExtra.h"
+
+class MyEffect : public APDI::Effect
+{
+public:
+    MyEffect(const Parameters& parameters, const Presets& presets); // constructor (initialise variables, etc.)
+    ~MyEffect();                                                    // destructor (clean up, free memory, etc.)
+
+    void setSampleRate(float sampleRate){ stk::Stk::setSampleRate(sampleRate); }
+    float getSampleRate() const { return stk::Stk::sampleRate(); };
+    
+    void process(const float** inputBuffers, float** outputBuffers, int numSamples);
+    
+    void presetLoaded(int iPresetNum, const char *sPresetName);
+    void optionChanged(int iOptionMenu, int iItem);
+    void buttonPressed(int iButton);
+    float fAval0;
+    float fAval1;
+    float fOutMultiplier;
+    float fOutMultiplierOld;
+    float fMax;
+    float fHoldCounter;
+    
+    float secToValue(float attackTime) {
+        float attackValue;
+        attackValue = ((getSampleRate() / attackTime) / 1000) / iMeasuredLength; // function calculates the amount of samples per s in the first brackets, then divides by 1000 to convert to ms, then divides my the length of the buffer to get the value to be added to a range of 0 - 1 in order to reach 0 or 1
+        
+        return attackValue;
+        
+    }
+    
+    bool bHighPass;
+    bool bBandPass;
+    bool bLowPass;
+    
+
+private:
+    // Declare shared member variables here
+    float *pfCircularBuffer;
+    float fSR;
+    int iMeasuredLength, iMeasuredItems;
+    float fMax0, fMax1, fMaxOldL, fMaxOldR;
+    int iBufferSize, iBufferWritePos;
+    
+};
+
+
+
